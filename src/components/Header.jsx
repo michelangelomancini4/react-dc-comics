50ff6d570d722a7f8b974bcc88c459b49213df86
michelangelomancini <michelangelomancini8@gmail.com>
2025-02-12 16:21:20 +0100

    creazione grezza dell' header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,26 @@
 // Header.jsx
 export default function Header() {
-    return (<header><h1>sono header</h1> </header>);
+    return (<header>
+
+        <div>
+            <span>logo</span>
+        </div>
+        <div>
+            <nav>
+                <ul className="lista-nav">
+                    <li><a href="">CHARACTERS</a></li>
+                    <li><a href="">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">VIDEOS</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">SHOP</a></li>
+
+                </ul>
+            </nav>
+        </div>
+    </header>);
 }
