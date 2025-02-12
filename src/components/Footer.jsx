e4bb30d2f94269b86f06bfaf8fdfce72194d283e
michelangelomancini <michelangelomancini8@gmail.com>
2025-02-12 16:57:19 +0100

    creazione grezza del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,47 @@
 // Footer.jsx
 export default function Footer() {
-    return (<footer><span>--qui contenuto in fondo alla pagina(footer)--</span> </footer>);
+    return (<footer>
+        <div className="linkutili-footer">
+            <div>
+                <h3>DC COMICS</h3>
+                <ul>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Comics</a></li>
+                    <li><a href="">Movies</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">Games</a></li>
+                    <li><a href="">Videos</a></li>
+                    <li><a href="">News</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>SHOP</h3>
+                <ul>
+                    <li><a href="">Shop DC</a></li>
+                    <li><a href="">Shop DC Collectibles</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>DC</h3>
+                <ul>
+                    <li><a href="">Terms Of Use</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Ad Choices</a></li>
+                    <li><a href="">Advertising</a></li>
+                    <li><a href="">Jobs</a></li>
+                    <li><a href="">Subscriptions</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>SITES</h3>
+                <ul>
+                    <li><a href="">DC</a></li>
+                    <li><a href="">MAD Magazine</a></li>
+                    <li><a href="">DC Kids</a></li>
+                    <li><a href="">DC Universe</a></li>
+                    <li><a href="">DC Power Visa</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>);
 }
