f67f393e287d1534e1674f291b85504de7fbe2c8
michelangelomancini <michelangelomancini8@gmail.com>
2025-02-12 16:41:58 +0100

    creazione grezza del main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,14 @@
 // Main.jsx
 export default function Main() {
-    return (<main><p>--qui contenuto principale(main)--</p> </main>);
+    return (<main>
+        <p className="main-container">Qui andrà il contenuto principale</p>
+
+        <ul className="shop-list">
+            <li><a href="">DIGITAL COMICS</a></li>
+            <li><a href="">DC MERCHANDISE</a></li>
+            <li><a href="">SUBSCRIPTION</a></li>
+            <li><a href="">COMIC SHOP LOCATOR</a></li>
+            <li><a href="">DC POWER VISA</a></li>
+        </ul>
+    </main>);
 }
